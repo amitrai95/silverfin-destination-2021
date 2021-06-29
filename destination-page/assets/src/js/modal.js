9b99f5d8426df177315ae700643a1d7ad11547b7
Amit <>
2021-06-29 14:55:57 +0100

    added popup modal functionality

---
+++ destination-page/assets/src/js/modal.js
@@ -0,0 +1,18 @@
+$(function() {
+
+  const openModal = $(".destModal--open");
+  const closeModal = $(".destModal--close");
+  const modal = $(".destModal");
+
+  $(openModal).click(function(e){
+    e.preventDefault();
+    $(modal).addClass("active");
+    $("body").addClass("overlay overlay--grey");
+  });
+
+  $(closeModal).click(function(){
+    $(modal).removeClass("active");
+    $("body").removeClass("overlay overlay--grey")
+  });
+
+});
