0a34a591dfed6b1589bd2cf8d103c75095c275b3
Amit <>
2021-06-30 09:41:10 +0100

    hero complete

--- destination-page/modules/destination-hero.module/src/module.js
+++ destination-page/modules/destination-hero.module/src/module.js
@@ -9,35 +9,42 @@ $(function() {
     duration: 900
   });
 
-  sr.reveal('.destHero__shape--1', {
-    origin: 'bottom',
-    distance: '200px',
-    duration: 1500,
-    delay: 500,
-  });
-
-  sr.reveal('.destHero__shape--2', {
+  sr.reveal('.destHero__bg--2', {
     origin: 'bottom',
     distance: '100px',
-    delay: 800,
+    delay: 500,
     duration: 1500
   });
 
-  sr.reveal('.destHero__shape--3', {
+  sr.reveal('.destHero__bg--1', {
+    origin: 'bottom',
+    distance: '200px',
+    duration: 1500,
+    delay: 800,
+  });
+
+  sr.reveal('.destHero__bg--3', {
     origin: 'bottom',
     distance: '100px',
     delay: 1200,
     duration: 1500
   });
 
-  sr.reveal('.destHero__shape--4', {
+  sr.reveal('.destHero__bg--4', {
     origin: 'bottom',
     distance: '100px',
     delay: 1500,
     duration: 1500
   });
 
-  sr.reveal('.destHero__shape--5', {
+  sr.reveal('.destHero__bg--5', {
+    origin: 'bottom',
+    distance: '100px',
+    delay: 1800,
+    duration: 1500
+  });
+
+  sr.reveal('.destHero__bg--6', {
     origin: 'bottom',
     distance: '100px',
     delay: 1800,
