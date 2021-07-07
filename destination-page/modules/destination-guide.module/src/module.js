62682db0428b6ff4e29f7211ec2093da61992366
Amit <>
2021-07-07 16:12:10 +0100

    destination-guide complete

--- destination-page/modules/destination-guide.module/src/module.js
+++ destination-page/modules/destination-guide.module/src/module.js
@@ -5,14 +5,29 @@ $(function() {
   sr.reveal('.destGuide__intro', {
     origin: 'bottom',
     distance: '100px',
-    duration: 2000,
-    duration: 900
+    duration: 1500,
+    delay: 500
   });
 
-  sr.reveal('.destGuide__bg--2', {
+  sr.reveal('.destGuide__guide', {
     origin: 'bottom',
     distance: '100px',
-    delay: 500,
+    duration: 1600,
+    delay: 600
+  });
+
+  sr.reveal('.destGuide__button', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1800,
+    delay: 800
+  });
+
+
+  sr.reveal('.destGuide__bg--2, .destGuide__bg--4', {
+    origin: 'bottom',
+    distance: '100px',
+    delay: 1500,
     duration: 1500
   });
 
@@ -27,13 +42,6 @@ $(function() {
     origin: 'bottom',
     distance: '100px',
     delay: 1200,
-    duration: 1500
-  });
-
-  sr.reveal('.destGuide__bg--4', {
-    origin: 'bottom',
-    distance: '100px',
-    delay: 1500,
     duration: 1500
   });
 
